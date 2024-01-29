9e0c9d5de9cc1cbc5e3d447d30fe5468fc9bbc34
Andrew Rabert <ar@nullsum.net>
2024-01-28 20:06:29 -0500

    Refactor

--- linode_dynamic_dns.py
+++ linode_dynamic_dns.py
@@ -14,15 +14,30 @@ __version__ = '0.6.2'
 APP_NAME = 'linode-dynamic-dns'
 LOGGER = logging.getLogger(APP_NAME)
 
-TIMEOUT = 15
-
-IP_URLS = {4: os.environ.get('IPV4_URL', 'https://ipv4.icanhazip.com'),
-           6: os.environ.get('IPV6_URL', 'https://ipv6.icanhazip.com')}
-
-LINODE_API_URL = 'https://api.linode.com/v4'
+DEFAULT_IPV4_URL = 'https://ipv4.icanhazip.com'
+DEFAULT_IPV6_URL = 'https://ipv6.icanhazip.com'
 
 
 class LinodeAPI:
+    # taken from https://www.linode.com/docs/api/domains/
+    VALID_DNS_TTL = [
+        300,
+        3600,
+        7200,
+        14400,
+        28800,
+        57600,
+        86400,
+        172800,
+        345600,
+        604800,
+        1209600,
+        2419200,
+    ]
+
+    URL = 'https://api.linode.com'
+    TIMEOUT = 15
+
     def __init__(self, key):
         self._key = key
 
@@ -38,28 +53,28 @@ class LinodeAPI:
             data = None
 
         request = urllib.request.Request(
-            url=f'{LINODE_API_URL}/{path}',
+            url=f'{self.URL}/{path}',
             headers=headers,
             method=method,
             data=data
         )
-        with urllib.request.urlopen(request, timeout=TIMEOUT) as response:
+        with urllib.request.urlopen(request, timeout=self.TIMEOUT) as response:
             return response.status, json.loads(response.read())
 
     def get_domains(self):
-        status, content = self.request('GET', 'domains')
+        status, content = self.request('GET', 'v4/domains')
         # TODO: Support pagination
         yield from content['data']
 
     def get_domain_records(self, domain_id):
-        status, content = self.request('GET', f'domains/{domain_id}/records')
+        status, content = self.request('GET', f'v4/domains/{domain_id}/records')
         yield from content['data']
 
     def update_domain_record_target(self, domain_id, record_id, target,
                                     ttl_sec):
         status, _ = self.request(
             'PUT',
-            f'domains/{domain_id}/records/{record_id}',
+            f'v4/domains/{domain_id}/records/{record_id}',
             json={'target': str(target),
                   'ttl_sec': ttl_sec}
         )
@@ -69,7 +84,7 @@ class LinodeAPI:
     def create_domain_record(self, domain_id, host, record_type, target, ttl_sec):
         status, _ = self.request(
             'POST',
-            f'domains/{domain_id}/records',
+            f'v4/domains/{domain_id}/records',
             json={
                 'name': host,
                 'type': record_type,
@@ -83,28 +98,39 @@ class LinodeAPI:
     def delete_domain_record(self, domain_id, record_id):
         status, _ = self.request(
             'DELETE',
-            f'domains/{domain_id}/records/{record_id}',
+            f'v4/domains/{domain_id}/records/{record_id}',
         )
         if status != 200:
             raise http.client.HTTPException(f'status {status}')
 
 
-def get_ip(version):
-    url = IP_URLS[version]
-    with urllib.request.urlopen(url, timeout=TIMEOUT) as response:
-        if response.status >= 400:
-            raise http.client.HTTPException(f'status {response.status}')
-        content = response.read()
-    ip = ipaddress.ip_address(content.decode().strip())
-    if ip and ip.version == version:
-        LOGGER.info(f'Local IPv{version} "{ip}"')
-        return ip
-    else:
-        LOGGER.info(f'No local IPv{version}.')
-        return None
+class IPLookup:
+    TIMEOUT = 15
+
+    def __init__(self, ipv4_url, ipv6_url):
+        self.ipv4_url = ipv4_url
+        self.ipv6_url = ipv6_url
+
+    def _request(self, url, version):
+        with urllib.request.urlopen(url, timeout=self.TIMEOUT) as response:
+            if response.status >= 400:
+                raise http.client.HTTPException(f'status {response.status}')
+            content = response.read()
+        ip = ipaddress.ip_address(content.decode().strip())
+        if ip and ip.version == version:
+            LOGGER.info(f'Local IPv{version} "{ip}"')
+            return ip
+        else:
+            raise RuntimeError(f'No local IPv{version}')
+
+    def get_ipv4(self):
+        return self._request(self.ipv4_url, version=4)
+
+    def get_ipv6(self):
+        return self._request(self.ipv4_url, version=6)
 
 
-def update_dns(api, domain, host, disable_ipv4, disable_ipv6, ttl,
+def update_dns(api, domain, host, disable_ipv4, disable_ipv6, ttl, iplookup,
                local_ipv4=None, local_ipv6=None):
     domain_id = None
     for d in api.get_domains():
@@ -134,7 +160,7 @@ def update_dns(api, domain, host, disable_ipv4, disable_ipv6, ttl,
         for record in record_a:
             _delete_record(api, domain_id, record)
     else:
-        local_ip = local_ipv4 or get_ip(4)
+        local_ip = local_ipv4 or iplookup.get_ipv4()
         if record_a:
             _update_record(api, domain_id, local_ip, ttl, record_a[0])
             for record in record_a[1:]:
@@ -146,7 +172,7 @@ def update_dns(api, domain, host, disable_ipv4, disable_ipv6, ttl,
         for record in record_aaaa:
             _delete_record(api, domain_id, record)
     else:
-        local_ip = local_ipv6 or get_ip(6)
+        local_ip = local_ipv6 or iplookup.get_ipv6()
         if record_aaaa:
             _update_record(api, domain_id, local_ip, ttl, record_aaaa[0])
             for record in record_aaaa[1:]:
@@ -209,80 +235,119 @@ def strtobool(value):
         raise ValueError(f'must be one of {_TRUE_VALUES + _FALSE_VALUES}')
 
 
-# taken from https://www.linode.com/docs/api/domains/
-_VALID_TTL = [
-    300,
-    3600,
-    7200,
-    14400,
-    28800,
-    57600,
-    86400,
-    172800,
-    345600,
-    604800,
-    1209600,
-    2419200,
-]
-
 _DEFAULT_TTL = 300
 
 
 def _parse_ttl(value):
-    if value:
-        value = int(value)
-        if value not in _VALID_TTL:
-            raise ValueError(f'must be one of {_VALID_TTL}')
-    else:
-        value = _DEFAULT_TTL
+    value = int(value)
+    if value not in LinodeAPI.VALID_DNS_TTL:
+        raise ValueError(f'must be one of {LinodeAPI.VALID_DNS_TTL}')
     return value
 
 
 def main():
     parser = argparse.ArgumentParser(APP_NAME)
     parser.add_argument(
+        '--access-token',
+        default=os.environ.get('LINODE_ACCESS_TOKEN'),
+        help='(env: LINODE_ACCESS_TOKEN)')
+    parser.add_argument(
+        '--sleep',
+        type=int,
+        default=None,
+        help='Run continuously and sleep the specified number of seconds'
+    )
+    parser.add_argument(
         '--version',
         action='version',
         version=__version__
     )
-    parser.add_argument(
-        '-s',
-        type=int,
-        dest='sleep',
-        default=None,
-        help='Run continuously and sleep the specified number of seconds'
-    )
+
+    linode_group = parser.add_argument_group('DNS options')
+    linode_group.add_argument(
+        '--dns-domain',
+        default=os.environ.get('LINODE_DNS_DOMAIN'),
+        help='(env: LINODE_DNS_DOMAIN)')
+    linode_group.add_argument(
+        '--dns-hostname',
+        default=os.environ.get('LINODE_DNS_HOSTNAME') or '',
+        help='(env: LINODE_DNS_HOSTNAME)')
+    linode_group.add_argument(
+        '--dns-ttl',
+        default=os.environ.get('LINODE_DNS_TTL') or _DEFAULT_TTL,
+        help=f'(env: LINODE_DNS_TTL) (default: {_DEFAULT_TTL})')
+
+    ipv4_group = parser.add_argument_group('IPv4 options')
+    ipv4_group.add_argument(
+        '--ipv4-url',
+        default=os.environ.get('IPV4_URL') or DEFAULT_IPV4_URL,
+        help=f'(env: IPV4_URL) (default: {DEFAULT_IPV4_URL})')
+    ipv4_group.add_argument(
+        '--ipv4-disable',
+        action='store_const',
+        const='true',
+        default=os.environ.get('IPV4_DISABLE', ''),
+        help='(env: IPV4_DISABLE)')
+    ipv4_group.add_argument(
+        '--ipv4-address',
+        default=os.environ.get('IPV4_ADDRESS'),
+        help='(env: IPV4_ADDRESS)')
+
+    ipv6_group = parser.add_argument_group('IPv6 options')
+    ipv6_group.add_argument(
+        '--ipv6-url',
+        default=os.environ.get('IPV6_URL') or DEFAULT_IPV6_URL,
+        help=f'(env: IPV6_URL) (default: {DEFAULT_IPV6_URL})')
+    ipv6_group.add_argument(
+        '--ipv6-disable',
+        action='store_const',
+        const='true',
+        default=os.environ.get('IPV6_DISABLE', ''),
+        help='(env: IPV6_DISABLE)')
+    ipv6_group.add_argument(
+        '--ipv6-address',
+        default=os.environ.get('IPV6_ADDRESS'),
+        help='(env: IPV6_ADDRESS)')
+
     args = parser.parse_args()
 
     logging.basicConfig(format='%(message)s', level=logging.INFO)
 
-    domain = os.environ['LINODE_DNS_DOMAIN']
-    host = os.environ.get('LINODE_DNS_HOSTNAME', '')
-    token = os.environ['LINODE_ACCESS_TOKEN']
-    ttl = _parse_ttl(os.environ.get('LINODE_DNS_TTL'))
-    disable_ipv4 = strtobool(os.environ.get('IPV4_DISABLE', ''))
-    disable_ipv6 = strtobool(os.environ.get('IPV6_DISABLE', ''))
-    local_ipv4 = os.environ.get('IPV4_ADDRESS')
-    local_ipv6 = os.environ.get('IPV6_ADDRESS')
+    if not args.dns_domain:
+        parser.error('must specify DNS domain')
+
+    if not args.access_token:
+        parser.error('must specify access token')
+
+    ttl = _parse_ttl(args.dns_ttl)
+
+    disable_ipv4 = strtobool(args.ipv4_disable)
+    disable_ipv6 = strtobool(args.ipv6_disable)
+
+    local_ipv4 = args.ipv4_address
+    local_ipv6 = args.ipv6_address
+
     if local_ipv4 is not None:
         local_ipv4 = ipaddress.ip_address(local_ipv4)
         if local_ipv4.version != 4:
-            raise ValueError('invalid IPv4 address')
+            parser.error('invalif IPv4 address')
     if local_ipv6 is not None:
         local_ipv6 = ipaddress.ip_address(local_ipv6)
         if local_ipv6.version != 6:
-            raise ValueError('invalid IPv6 address')
+            parser.error('invalid IPv6 address')
 
-    api = LinodeAPI(token)
+    api = LinodeAPI(args.access_token)
+    iplookup = IPLookup(ipv4_url=args.ipv4_url, ipv6_url=args.ipv6_url)
 
     while True:
         update_dns(
             api,
-            domain,
-            host,
+            args.dns_domain,
+            args.dns_hostname,
             disable_ipv4=disable_ipv4,
             disable_ipv6=disable_ipv6,
             ttl=ttl,
+            iplookup=iplookup,
             local_ipv4=local_ipv4,
             local_ipv6=local_ipv6,
         )
